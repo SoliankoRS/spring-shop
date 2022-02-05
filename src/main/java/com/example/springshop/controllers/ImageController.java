8aa23895a78e2c14373162deeccd71ac793e871d
Roman Solianko <rsolianko@gmail.com>
2022-02-05 20:17:30 +0300

    Change request mapping in admin and image controllers.

--- src/main/java/com/example/springshop/controllers/ImageController.java
+++ src/main/java/com/example/springshop/controllers/ImageController.java
@@ -8,19 +8,22 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.ByteArrayInputStream;
 
 @RestController
+@RequestMapping("/images")
 @RequiredArgsConstructor
 public class ImageController {
 
     private final ImageRepository imageRepository;
 
-    @GetMapping("/images/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<?> getImageById(@PathVariable Long id) {
         Image image = imageRepository.findById(id).orElse(null);
+        assert image != null;
         return ResponseEntity.ok()
                 .header("fileName", image.getOriginalFileName())
                 .contentType(MediaType.valueOf(image.getContentType()))
